308badaf575704737fdd4ca3f26b26b0fe41d251
Goksenin2 <ec24627@qmul.ac.uk>
2025-07-03 20:45:08 +0300

    Create Student.java

---
+++ src/main/java/demo/Student.java
@@ -0,0 +1,23 @@
+package demo;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private int id;
+    private String name;
+
+    public Student() { }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name='" + name + "'}";
+    }
+}
